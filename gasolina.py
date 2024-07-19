e846999bcf848e5946c75fa89d36ed8a79cc93d7
rsgomes95 <raphael.gomes1995@gmail.com>
2024-07-19 01:22:56 +0000

    arquivo Gasolina.png alterado

---
+++ gasolina.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import seaborn as sns
+
+gasolina_df = pd.read_csv('gasolina.csv', sep=',')
+
+with sns.axes_style('whitegrid'):
+
+  grafico = sns.lineplot(data=gasolina_df, x="dia", y="venda", palette="pastel")
+  grafico.set(title='Preço da gasolina', xlabel='Dia', ylabel='Preço');
